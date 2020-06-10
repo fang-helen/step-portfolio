2622975157c3cfe131f5a8d2eb546596a2c5f9af
Helen Fang <helenfang524@gmail.com>
2020-06-10 21:29:11 +0000

    Fix spacing

--- portfolio/src/main/java/com/google/sps/servlets/TranslateServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/TranslateServlet.java
@@ -18,7 +18,6 @@ public class TranslateServlet extends HttpServlet {
 
   private static final Logger LOGGER = Logger.getLogger(TranslateServlet.class.getName());
 
-
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String text = request.getParameter("text");
@@ -30,12 +29,8 @@ public class TranslateServlet extends HttpServlet {
         translate.translate(text, Translate.TranslateOption.targetLanguage(lang));
     String translatedText = translation.getTranslatedText();
 
-    // Output the translation.
     response.setContentType("text/html; charset=UTF-8");
     response.setCharacterEncoding("UTF-8");
     response.getWriter().println(translatedText);
-  
   }
-
-    
 }
