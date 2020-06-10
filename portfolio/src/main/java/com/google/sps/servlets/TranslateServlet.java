9b780b6d98d3901b52d66e23006d7b81d7a3e133
Helen Fang <helenfang524@gmail.com>
2020-06-10 19:36:57 +0000

    Create translate servlet

---
+++ portfolio/src/main/java/com/google/sps/servlets/TranslateServlet.java
@@ -0,0 +1,41 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet("/translate-data")
+public class TranslateServlet extends HttpServlet {
+
+  private static final Logger LOGGER = Logger.getLogger(TranslateServlet.class.getName());
+
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String text = request.getParameter("text");
+    String lang = request.getParameter("lang");
+    LOGGER.info("text: " + text + "; lang: " + lang);
+
+    Translate translate = TranslateOptions.getDefaultInstance().getService();
+    Translation translation =
+        translate.translate(text, Translate.TranslateOption.targetLanguage(lang));
+    String translatedText = translation.getTranslatedText();
+
+    // Output the translation.
+    response.setContentType("text/html; charset=UTF-8");
+    response.setCharacterEncoding("UTF-8");
+    response.getWriter().println(translatedText);
+  
+  }
+
+    
+}
